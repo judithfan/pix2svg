af754a42672772fadbe5080df6ef99c06a1deb06
mhw32 <me@mikewuis.me>
2017-08-10 17:45:31 -0700

    Added Suggested Improvements - Added patience to implicitly "stop" drawing based on the informativity metric (called loss in code) - Added distraction images / features to normalize the informativity metric - Added images that I will be using to test to data/ folder

--- generative/pix2sketch.py
+++ generative/pix2sketch.py
@@ -40,31 +40,11 @@ def build_vgg19_feature_extractor(vgg):
     return vgg_copy
 
 
-def gen_action(iter, min_iter=10, max_iter=20, min_stop_proba=0.0,
-               max_stop_proba=0.50, draw_move_ratio=0.75):
-    """Sample an action from an action space of draw, move, stop.
-    We reverse anneal the stop probability as the iter approaches
-    the maximum iteration.
-
-    :param iter: current iteration
-    :param min_iter: when to start reverse annealing stop probabilities
-    :param max_iter: when to reach the max stop probability
-    :param min_stop_proba: beginning stop probability
-    :param max_stop_proba: maximum stop probability
-    :param draw_move_ratio: fixed ratio of draw to move probability
+def gen_action():
+    """Sample an action from an action space of 'draw' or 'move'
+    For now, we simply always return 'draw'.
     """
-    if iter < min_iter:
-        stop_proba = min_stop_proba
-        draw_proba = (1.0 - stop_proba) * draw_move_ratio
-        move_proba = 1 - draw_proba - stop_proba
-    else:
-        stop_rate = (max_stop_proba - min_stop_proba) / (max_iter - min_iter)
-        stop_proba = min_stop_proba + iter * stop_rate
-        draw_proba = (1.0 - stop_proba) * draw_move_ratio
-        move_proba = 1 - draw_proba - stop_proba
-    action_proba = np.array([draw_proba, move_proba, stop_proba])
-    action_state = np.array(['draw', 'move', 'stop'])
-    return np.random.choice(action_state, 1, p=action_proba)[0]
+    return 'draw'
 
 
 def gen_canvas(x_list, y_list, action_list, outpath='./tmp.svg', size=256):
@@ -72,13 +52,13 @@ def gen_canvas(x_list, y_list, action_list, outpath='./tmp.svg', size=256):
 
     :param x_list: list of floats - x coordinates
     :param y_list: list of floats - y coordinates
-    :param action_list: list of strings ('draw', 'move', 'stop')
+    :param action_list: list of strings ('draw', 'move')
     :param outpath: string - where to save canvas
     :param size: size of the canvas
     """
     assert len(x_list) == len(y_list)
     assert len(x_list) == len(action_list) + 1
-    assert set(action_list) <  set(['draw', 'move', 'stop'])
+    assert set(action_list) <  set(['draw', 'move'])
 
     # initialize canvas
     size ='{}px'.format(size)
@@ -87,7 +67,7 @@ def gen_canvas(x_list, y_list, action_list, outpath='./tmp.svg', size=256):
                  size=(size, size),
                  fill='white'))
 
-    # start with first point and draw/move/stop a segment
+    # start with first point and draw/move a segment
     num_coords = len(x_list)
     for i in range(num_coords - 1):
         x_s, y_s = x_list[i], y_list[i]
@@ -97,8 +77,6 @@ def gen_canvas(x_list, y_list, action_list, outpath='./tmp.svg', size=256):
                                                          x_e=x_e, y_e=y_e)
         elif action_list[i] == 'move':
             path = "M {x_e},{y_e} ".format(x_e=x_e, y_e=y_e)
-        else:  # 'stop'
-            continue
         # perform the action
         dwg.add(dwg.path(path).stroke("black", 3))
 
@@ -120,8 +98,8 @@ def svg2png(svgpath, width=256, height=256):
     return pngpath
 
 
-def load_canvases(paths):
-    """Load a bunch saved canvas.svg into a Torch environment
+def load_images_to_torch(paths):
+    """Load a bunch jpegs/pngs/etc. into a Torch environment
     as a single Torch Variable.
 
     :param paths: list of str
@@ -161,22 +139,20 @@ def sample_endpoints(x_s, y_s, std=10, size=1, min_x=0, max_x=256, min_y=0, max_
     return samples
 
 
-def compute_losses(natural_image, sketch_images, vgg_features,
-                   label=None, label_weight=1.0, vgg_scores=None):
+def compute_losses(natural_image, sketch_images, distraction_images, vgg_features):
     """Compute a loss to describe how semantically far apart
     the natural image and the sketch image are. If label is not
     None, compute an additional penalty for getting the label wrong.
 
-    loss = distance(natural_image, sketch_image) + label_weight*xentropy(proba, labels)
+    loss = distance(natural_image, sketch_image, distraction_images)
 
-    Compute the losses for each (natural_image, sketch_image) pair.
+    Compute the losses for each (natural_image, sketch_image) pair and
+    normalize each loss by each (distraction_image, sketch_image) pair
 
     :param natural_image: PyTorch Variable (1 x C x H x W)
-    :param sketch_image: PyTorch Variable (N x C x H x W)
+    :param sketch_images: PyTorch Variable (N x C x H x W)
+    :param distraction_images: PyTorch Variable (M x C x H x W)
     :param vgg_features: VGG19 PyTorch instance that produces features
-    :param label: float - a number from 0 to # classes - 1
-    :param label_weight: regularization parameter to weight xentropy
-    :param vgg_scores: VGG19 PyTorch instance that produces scores
     """
 
     def extract_features(data):
@@ -192,6 +168,10 @@ def compute_losses(natural_image, sketch_images, vgg_features,
         return scores / np.linalg.norm(scores, axis=1)[:, None]
 
     natural_features = extract_features(natural_image)
+    # compute features for all distraction images (not batching since this is a small number)
+    num_distractions = distraction_images.size()[0]
+    distraction_features_arr = extract_features(distraction_images)
+
     # compute features for all sketches in batches (if batch is too
     # large then we will have memory issues)
     num_sketches = sketch_images.size()[0]  # number of images total
@@ -218,21 +198,18 @@ def compute_losses(natural_image, sketch_images, vgg_features,
     # stack all of them together
     sketch_features_arr = np.vstack(sketch_features_arr)
 
-    if label is not None and vgg_scores is not None:
-        # if we are weighting the class, compute all scores at once
-        # TODO: convert me to batch based if i'm going to use this
-        sketch_class_probas_arr = extract_scores(sketch_images)
-
     # for each sketch, compute loss with natural image
     losses = np.zeros(num_sketches)
     for i in range(num_sketches):
-        dist = 1 - cosine(natural_features, sketch_features_arr[i])
-        if label is not None and vgg_scores is not None:
-            dist += label_weight * F.nll_loss(
-                Variable(torch.from_numpy(sketch_class_probas_arr[i][np.newaxis])),
-                Variable(torch.from_numpy(np.array([label])))
-            ).exp().data.numpy()[0]
-        losses[i] = dist
+        natural_dist = 1 - cosine(natural_features, sketch_features_arr[i])
+        # these distractions serve as context such that we can ground the natural_dist metric
+        # into some interpretable space.
+        distraction_dists = []
+        for j in range(num_distractions):
+            distraction_dists.append(1 - cosine(distraction_features_arr[j], sketch_features_arr[i]))
+        # normalize natural_dist by distraction_dists
+        all_dists = np.array([natural_dist] + distraction_dists)
+        losses[i] = natural_dist / np.linalg.norm(all_dists)
 
     return losses
 
@@ -248,6 +225,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="generate sketches")
     parser.add_argument('--imagepath', type=str, help='path to image file')
+    parser.add_argument('--distractdir', type=str, help='directory to distractor image files')
     parser.add_argument('--sketchdir', type=str, help='directory to store sketches')
     parser.add_argument('--n_samples', type=int, default=100,
                         help='number of samples per iteration')
@@ -257,6 +235,8 @@ if __name__ == '__main__':
                         help='size when running forward pass (memory issues)')
     parser.add_argument('--std', type=float, default=15.0,
                         help='std for Gaussian when sampling')
+    parser.add_argument('--patience', type=int, default=5,
+                        help='once the informativity measure stops improving, wait N epochs before quitting')
     args = parser.parse_args()
 
     # pretrained on imagenet
@@ -270,15 +250,23 @@ if __name__ == '__main__':
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
+    # load the natural image we want to sketch
     img = Image.open(args.imagepath)
     natural = Variable(preprocessing(img).unsqueeze(0))
+
+    # load the distractions that we use to normalize our metric
+    distraction_paths = os.listdir(args.distractdir)
+    distractions = load_images_to_torch(distraction_paths)
 
     # cut off part of the net to generate features
     vgg_ext = build_vgg19_feature_extractor(vgg19)
 
     x_e, y_e = 128, 128  # start in center
     x_selected, y_selected, action_selected = [x_e], [y_e], []
-    loss_per_epoch = []
+    loss_per_iter = []
+    best_loss = 0.0  # this will be updated
+    best_loss_iter = 0  # which epoch had best loss
+    patience = args.patience
 
     for iter in range(args.n_iters):
         print('ITER (%i/%i):' % (iter + 1, args.n_iters))
@@ -286,8 +274,7 @@ if __name__ == '__main__':
                                          min_x=0, max_x=256, min_y=0, max_y=256)
         x_samples, y_samples = coord_samples[:, 0], coord_samples[:, 1]
         print('- generated %i samples' % args.n_samples)
-        # action_sample = gen_action(1)
-        action_sample = 'draw'
+        action_sample = gen_action()
         print('- sampled \'%s\' action' % action_sample)
 
         svg_paths = []
@@ -307,15 +294,29 @@ if __name__ == '__main__':
             png_paths.append(png_path)
         print('- converted to %i png canvases' % args.n_samples)
 
-        sketches = load_canvases(png_paths)
-        losses = compute_losses(natural, sketches, vgg_ext)
+        sketches = load_images_to_torch(png_paths)
+        losses = compute_losses(natural, sketches, vgg_ext, distractions=distractions)
         winning_index = np.argmax(losses)
-        print('- calculated loss; best loss: %f' % losses[winning_index])
-        loss_per_epoch.append(losses[winning_index])
+        print('- calculated loss: %f' % losses[winning_index])
+        loss_per_iter.append(losses[winning_index])
 
         x_selected.append(x_samples[winning_index])
         y_selected.append(y_samples[winning_index])
         action_selected.append(action_sample)
+
+        if losses[winning_index] >= best_loss:
+            best_loss = losses[winning_index]
+            best_loss_iter = iter
+            patience = args.patience
+            print('- new best loss; patience reset to %d' % patience)
+        else:
+            patience -= 1
+            print('- patience lowered to %d' % patience)
+
+        if patience <= 0:  # this is equivalent to "stop" action
+            print('- out of patience. quitting...')
+            break
+
         print('')  # new line
 
     print('------------------------------')
@@ -327,8 +328,9 @@ if __name__ == '__main__':
 
     # save output to savedir
     save_path = os.path.join(args.sketchdir, 'sketch.svg')
-    svg_path = gen_canvas(x_selected, y_selected, action_selected,
-                          outpath=save_path)
+    # regenerate the canvas with strokes up to the best iter (before losing patience)
+    svg_path = gen_canvas(x_selected[:best_loss_iter+1], y_selected[:best_loss_iter+1],
+                          action_selected[:best_loss_iter], outpath=save_path)
     png_path = svg2png(svg_path)
     os.remove(svg_path)
     print('saved sketch to \'%s\'' % png_path)
@@ -336,7 +338,7 @@ if __name__ == '__main__':
     # save plot to pngs
     plot_path = os.path.join(args.sketchdir, 'distance_over_time.png')
     plt.figure()
-    plt.plot(loss_per_epoch)
+    plt.plot(loss_per_iter)
     plt.xlabel('epoch')
     plt.ylabel('distance')
     plt.tight_layout()
