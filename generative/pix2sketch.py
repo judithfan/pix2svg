bc97414f213e976a7a284eb385dc8670bc67f14d
mhw32 <me@mikewuis.me>
2017-08-14 00:26:09 -0700

    Added beam search to generative process; defaults to a beam width of 1 aka greedy search

--- generative/pix2sketch.py
+++ generative/pix2sketch.py
@@ -199,7 +199,7 @@ def compute_losses(natural_image, sketch_images, distraction_images, vgg_feature
     sketch_features_arr = np.vstack(sketch_features_arr)
 
     # for each sketch, compute loss with natural image
-    losses = np.zeros(num_sketches)
+    losses = [0]*num_sketches
     for i in range(num_sketches):
         natural_dist = 1 - cosine(natural_features, sketch_features_arr[i])
         # these distractions serve as context such that we can ground the natural_dist metric
@@ -224,9 +224,9 @@ if __name__ == '__main__':
     sns.set_style('whitegrid')
 
     parser = argparse.ArgumentParser(description="generate sketches")
-    parser.add_argument('--imagepath', type=str, help='path to image file')
-    parser.add_argument('--distractdir', type=str, help='directory to distractor image files')
-    parser.add_argument('--sketchdir', type=str, help='directory to store sketches')
+    parser.add_argument('--image_path', type=str, help='path to image file')
+    parser.add_argument('--distract_dir', type=str, help='directory to distractor image files')
+    parser.add_argument('--sketch_dir', type=str, help='directory to store sketches')
     parser.add_argument('--n_samples', type=int, default=100,
                         help='number of samples per iteration')
     parser.add_argument('--n_iters', type=int, default=20,
@@ -237,6 +237,8 @@ if __name__ == '__main__':
                         help='std for Gaussian when sampling')
     parser.add_argument('--patience', type=int, default=5,
                         help='once the informativity measure stops improving, wait N epochs before quitting')
+    parser.add_argument('--beam_width', type=int, default=1,
+                        help='number of particles to preserve at each timestep')
     args = parser.parse_args()
 
     # pretrained on imagenet
@@ -248,74 +250,130 @@ if __name__ == '__main__':
         transforms.Scale(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
     ])
 
     # load the natural image we want to sketch
-    img = Image.open(args.imagepath)
+    img = Image.open(args.image_path)
     natural = Variable(preprocessing(img).unsqueeze(0))
     print('loaded natural image')
 
     # load the distractions that we use to normalize our metric
-    distraction_paths = [os.path.join(args.distractdir, i)
-                         for i in os.listdir(args.distractdir)]
+    distraction_paths = [os.path.join(args.distract_dir, i)
+                         for i in os.listdir(args.distract_dir)]
     distractions = load_images_to_torch(distraction_paths)
     print('loaded distraction images\n')
 
     # cut off part of the net to generate features
     vgg_ext = build_vgg19_feature_extractor(vgg19)
 
-    x_e, y_e = 128, 128  # start in center
-    x_selected, y_selected, action_selected = [x_e], [y_e], []
-    loss_per_iter = []
-    best_loss = 0.0  # this will be updated
-    best_loss_iter = 0  # which epoch had best loss
+    x_init, y_init = 128, 128  # start in center
+    # store the best & average beam loss as we traverse
+    best_loss_per_iter, average_loss_per_iter = [], []
+
+    # variables for early-stopping by loss
+    best_loss = 0.0
+    best_loss_iter = 0
+    best_loss_beam = 0
     patience = args.patience
 
+    # each queue holds future paths to try
+    # for the first iteration, use beam_width repetitions of the init point
+    # - a little inefficient but simple!
+    x_beam_queue, y_beam_queue = [x_init]*args.beam_width, [y_init]*args.beam_width
+    # store full path for top particles
+    x_beam_paths = [[x_init] for b in range(args.beam_width)]
+    y_beam_paths = [[y_init] for b in range(args.beam_width)]
+    action_beam_paths = [[] for b in range(args.beam_width)]
+
     for iter in range(args.n_iters):
-        print('ITER (%i/%i):' % (iter + 1, args.n_iters))
-        coord_samples = sample_endpoints(x_e, y_e, std=args.std, size=args.n_samples,
-                                         min_x=0, max_x=256, min_y=0, max_y=256)
-        x_samples, y_samples = coord_samples[:, 0], coord_samples[:, 1]
-        print('- generated %i samples' % args.n_samples)
-        action_sample = gen_action()
-        print('- sampled \'%s\' action' % action_sample)
+        assert len(x_beam_queue) == len(y_beam_queue)
+        print('ITER (%d/%d) - BEAM QUEUE LENGTH: %d' %
+              (iter + 1, args.n_iters, args.beam_width))
 
-        svg_paths = []
-        for i in range(args.n_samples):
-            x_path = x_selected + [x_samples[i]]
-            y_path = y_selected + [y_samples[i]]
-            action_path = action_selected + [action_sample]
-            svg_path = gen_canvas(x_path, y_path, action_path,
-                                  outpath=os.path.join(args.sketchdir, '{}.svg'.format(i)))
-            svg_paths.append(svg_path)
-        print('- generated %i svg canvases' % args.n_samples)
+        beam_losses = []  # stores losses across all beams
+        x_beam_samples, y_beam_samples, action_beam_samples = [], [], []
 
-        # do this in a separate for loop to avoid conflicts in render time
-        png_paths = []  # convert svgs to pngs
-        for i in range(args.n_samples):
-            png_path = svg2png(os.path.join(args.sketchdir, '{}.svg'.format(i)))
-            png_paths.append(png_path)
-        print('- converted to %i png canvases' % args.n_samples)
+        for b in range(args.beam_width):
+            # load particle coordinates
+            coord_samples = sample_endpoints(x_beam_queue[b], y_beam_queue[b],
+                                             std=args.std, size=args.n_samples,
+                                             min_x=0, max_x=256, min_y=0, max_y=256)
+            x_samples, y_samples = coord_samples[:, 0].tolist(), coord_samples[:, 1].tolist()
+            print('- generated %d samples' % args.n_samples)
 
-        sketches = load_images_to_torch(png_paths)
-        losses = compute_losses(natural, sketches, distractions, vgg_ext)
-        winning_index = np.argmax(losses)
-        print('- calculated loss: %f' % losses[winning_index])
-        loss_per_iter.append(losses[winning_index])
+            action_sample = gen_action()  # TODO: make probabilistic
+            print('- sampled \'%s\' action' % action_sample)
 
-        x_selected.append(x_samples[winning_index])
-        y_selected.append(y_samples[winning_index])
-        action_selected.append(action_sample)
+            svg_paths = []  # try out each sample for the current particle
+            for i in range(args.n_samples):
+                x_path = x_beam_paths[b] + [x_samples[i]]
+                y_path = y_beam_paths[b] + [y_samples[i]]
+                action_path = action_beam_paths[b] + [action_sample]
+                # create svg drawing
+                svg_path = gen_canvas(x_path, y_path, action_path,
+                                      outpath=os.path.join(args.sketch_dir,
+                                                           '{}.svg'.format(i)))
+                svg_paths.append(svg_path)
+            print('- generated %i svg canvases' % args.n_samples)
 
-        if losses[winning_index] >= best_loss:
-            best_loss = losses[winning_index]
+            # convert svg to pngs
+            png_paths = [svg2png(svg_paths[i]) for i in range(args.n_samples)]
+            print('- converted to %i png canvases' % args.n_samples)
+
+            sketches = load_images_to_torch(png_paths)
+            losses = compute_losses(natural, sketches, distractions, vgg_ext)
+
+            beam_losses += losses
+            x_beam_samples += x_samples
+            y_beam_samples += y_samples
+            action_beam_samples.append(action_sample)
+
+            print('Finished beam particle %d\n' % (b + 1))
+
+        # make them all numpy arrays for easy indexing
+        beam_losses = np.array(beam_losses)
+        x_beam_samples = np.array(x_beam_samples)
+        y_beam_samples = np.array(y_beam_samples)
+        action_beam_samples = np.array(action_beam_samples)
+
+        # keep the top N particles
+        top_indexes = np.argsort(beam_losses)[::-1][:args.beam_width]
+
+        # these will hold updated beam paths
+        _x_beam_paths, _y_beam_paths, _action_beam_paths = [], [], []
+        for b in range(args.beam_width):  # update our beam paths
+            beam_parent = top_indexes[b] % args.n_samples  # which beam it originated from
+            _x_beam_paths[b] = x_beam_paths[beam_parent] + [x_beam_samples[top_indexes[b]]]
+            _y_beam_paths[b] = x_beam_paths[beam_parent] + [y_beam_samples[top_indexes[b]]]
+            _action_beam_paths[b] = action_beam_paths[beam_parent] + [action_beam_samples[b]]
+
+        # overwrite old paths with new ones
+        x_beam_paths = _x_beam_paths
+        y_beam_paths = _y_beam_paths
+        action_beam_paths = _action_beam_paths
+
+        # update beam queue with top beam_width samples across beams
+        x_beam_queue = [x_beam_samples[top_indexes[b]] for b in args.beam_width]
+        y_beam_queue = [y_beam_samples[top_indexes[b]] for b in args.beam_width]
+
+        # save loss statistics
+        top_beam_loss = beam_losses[top_indexes[0]]
+        average_beam_loss = np.mean(beam_losses[top_indexes])
+        best_loss_per_iter.append(top_beam_loss)
+        average_loss_per_iter.append(average_beam_loss)
+
+        # update patience constants (and possibly break)
+        if top_beam_loss >= best_loss:
+            best_loss = top_beam_loss
+            best_loss_beam = top_indexes[0] % args.n_samples
             best_loss_iter = iter
             patience = args.patience
-            print('- new best loss; patience reset to %d' % patience)
+            print('- new best loss %f. patience reset to %d' % (best_loss, patience))
         else:
             patience -= 1
-            print('- patience lowered to %d' % patience)
+            print('- no improvement. patience lowered to %d' % patience)
 
         if patience <= 0:  # this is equivalent to "stop" action
             print('- out of patience. quitting...')
@@ -331,35 +389,24 @@ if __name__ == '__main__':
     print('deleted %i generated files' % len(generated_paths))
 
     # save output to savedir
-    save_path = os.path.join(args.sketchdir, 'sketch.svg')
+    save_path = os.path.join(args.sketch_dir, 'sketch.svg')
     # regenerate the canvas with strokes up to the best iter (before losing patience)
-    svg_path = gen_canvas(x_selected[:best_loss_iter+1], y_selected[:best_loss_iter+1],
-                          action_selected[:best_loss_iter], outpath=save_path)
+    svg_path = gen_canvas(x_beam_paths[best_loss_beam][:best_loss_iter+1],
+                          y_beam_paths[best_loss_beam][:best_loss_iter+1],
+                          action_beam_paths[best_loss_beam][:best_loss_iter],
+                          outpath=save_path)
     png_path = svg2png(svg_path)
     os.remove(svg_path)
     print('saved sketch to \'%s\'' % png_path)
 
-    # save the internal components:
-    output_path = os.path.join(args.sketchdir, 'sketch_outputs.npy')
-    np.save(
-        output_path,
-        {
-            'x': x_selected,
-            'y': y_selected,
-            'action': action_selected,
-            'best_iter': best_loss_iter,
-            'best_loss': best_loss,
-            'patience': patience,
-        },
-    )
-    print('saved output variables to \'%s\'' % output_path)
-
     # save plot to pngs
-    plot_path = os.path.join(args.sketchdir, 'distance_over_time.png')
+    plot_path = os.path.join(args.sketch_dir, 'distance_over_time.png')
     plt.figure()
-    plt.plot(loss_per_iter)
+    plt.plot(best_loss_per_iter, label='best')
+    plt.plot(average_loss_per_iter, label='average')
     plt.xlabel('epoch')
     plt.ylabel('distance')
     plt.tight_layout()
+    plt.legend()
     plt.savefig(plot_path)
     print('saved losses to \'%s\'' % plot_path)
