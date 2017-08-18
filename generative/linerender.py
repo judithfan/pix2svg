372f2e6d984f7352fd3ffe0777cae84690d3f79b
mhw32 <me@mikewuis.me>
2017-08-18 09:51:08 -0700

    Added ReLU blurring to render net operation

--- generative/linerender.py
+++ generative/linerender.py
@@ -26,47 +26,53 @@ class RenderNet(nn.Module):
     """
     def __init__(self, x0, y0, x1, y1, imsize=256, linewidth=1):
         super(RenderNet, self).__init__()
-        self.x0 = Variable(torch.FloatTensor([x0]), requires_grad=False)
-        self.y0 = Variable(torch.FloatTensor([y0]), requires_grad=False)
-        self.x1 = Parameter(torch.FloatTensor([x1]))
-        self.y1 = Parameter(torch.FloatTensor([y1]))
+        self.x0 = Variable(torch.Tensor([x0]), requires_grad=False)
+        self.y0 = Variable(torch.Tensor([y0]), requires_grad=False)
+        self.x1 = Parameter(torch.Tensor([x1]))
+        self.y1 = Parameter(torch.Tensor([y1]))
         self.imsize = imsize
         self.linewidth = linewidth
 
+    def gen_kernel(self):
+        linewidth = self.linewidth
+        center = linewidth // 2
+        kernel = torch.zeros((3, 3, linewidth, linewidth))
+        # compose our kernel out of smaller kernels
+        for i in range(center + 1):
+            width = linewidth - i * 2
+            value = (1.0 / (center + 1)) * (i + 1)
+            _kernel = torch.ones((width, width)) * value
+
+            offset = (linewidth - width) // 2
+            kernel[
+                :,
+                :,
+                offset:linewidth - offset,
+                offset:linewidth - offset,
+            ] = _kernel
+        return Variable(kernel)
+
     def forward(self):
-        dx = torch.abs(self.x1 - self.x0)
-        dy = torch.abs(self.y1 - self.y0)
-        m = dy / dx
-
-        # evenly space out the elements (linspace)
-        xpix = Variable(torch.arange(0, dx.data[0] + 1)) + x0
-        ypix = Variable(torch.arange(0, dy.data[0] + 1)) + y0
-
-        # sort xpix in increasing order
-        xpix, sorted_indices = torch.sort(xpix)
-        ypix = ypix[sorted_indices]
-
         # render the points as a CHW Tensor
-        xmin = int(torch.min(xpix).data[0])
-        xmax = int(torch.max(xpix).data[0])
+        xmin = int(torch.min(self.x0, self.x1).data[0])
+        xmax = int(torch.max(self.x0, self.x1).data[0])
+        ymin = int(torch.min(self.y0, self.y1).data[0])
+        ymax = int(torch.max(self.y0, self.y1).data[0])
+        slope = (ymax - ymin) / (xmax - xmin)
 
-        templates = []  # hold lots of vectors
-        for y in ypix.split(1):
-            y = y.long()
-            r = Variable(torch.zeros(self.imsize))
-            r[y] = 255  # one hot the relevant index
-            templates.append(r)
+        padding = self.linewidth - 1
+        templates = Variable(torch.ones((3, self.imsize + padding,
+                                         self.imsize + padding)))
+        for i in range(xmin, xmax + 1):
+            x = i + padding
+            y = int(round(x * slope + ymin)) + padding
+            templates[:, x, y] = 0
 
-        templates = torch.t(torch.stack(templates))
-        templates = torch.stack((templates, templates, templates), dim=0)
+        kernel = self.gen_kernel()
+        templates = F.conv2d(torch.unsqueeze(templates, dim=0), kernel)
 
-        # prepend and postpend unmanipulated sections
-        if xmin > 0:
-            prefix = Variable(torch.zeros((3, self.imsize, xmin)))
-            templates = torch.stack((prefix, templates))
-        if xmax < self.imsize:
-            postfix = Variable(torch.zeros((3, self.imsize,
-                                           self.imsize - xmax - 1)))
-            templates = torch.cat((templates, postfix), dim=2)
+        template_min = torch.min(templates)
+        template_max = torch.max(templates)
+        templates = (templates - template_min) / (template_max - template_min)
 
         return templates
