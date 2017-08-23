resolved 0 gradient bug with indexing using a variable + added pixel wise loss

generative/linerender.py
@@ -30,9 +30,6 @@ class RenderNet(nn.Module):
         self.y0 = Variable(torch.Tensor([y0]))
         self.x1 = Parameter(torch.Tensor([x1]))
         self.y1 = Parameter(torch.Tensor([y1]))
-        # save raw values for indexing
-        self._x1 = x1
-        self._y1 = y1
         self.imsize = imsize
         if linewidth % 2 == 0:
             linewidth += 1
@@ -42,12 +39,12 @@ class RenderNet(nn.Module):
     def gen_kernel(self):
         linewidth = self.linewidth
         center = linewidth // 2
-        kernel = torch.zeros((3, 3, linewidth, linewidth))
+        kernel = torch.zeros((1, 1, linewidth, linewidth))
         # compose our kernel out of smaller kernels
         for i in range(center + 1):
             width = linewidth - i * 2
             value = (1.0 / (center + 1)) * (i + 1)
-            _kernel = torch.ones((3, 3, width, width)) * value
+            _kernel = torch.ones((1, 1, width, width)) * value
             offset = (linewidth - width) // 2
             kernel[
                 :,
@@ -57,24 +54,30 @@ class RenderNet(nn.Module):
             ] = _kernel
         return Variable(kernel)
 
-    def smooth_index(self, template, x, y, _x, _y):
+    def smooth_index(self, template, kernel, x, y, _x, _y, value):
         """Indexing using Variable, even with scatter_ is not
         differentiable. This is our hacky (& slower) workaround.
 
         :param template: torch matrix (imsize x imsize)
+        :param kernel: torch matrix (dim x dim x kH x kW)
         :param x: torch Parameter for x coordinate
         :param y: torch Parameter for y coordinate
         :param _x: integer for x coordinate
         :param _y: integer for y coordinate
+        :param value: float value to set it to
         :return: None, in-place operation on template
         """
         z = x + y
-        mask = z.expand(self.imsize, self.imsize)
-        difference = Variable(torch.zeros(self.imsize, self.imsize))
-        difference[_x, _y] = -z
+        mask = z.expand(1, 1, self.imsize, self.imsize)
+        difference = Variable(torch.zeros(1, 1, self.imsize, self.imsize))
+        difference[:, :, _x, _y] = -z
         mask = z - torch.add(mask, difference)
-        mask = torch.mul(mask, 1 / z)
+        mask = torch.mul(mask, value / z)
+        mask = F.conv2d(mask, kernel, padding=self.linewidth // 2)
+        # convolve before adding
         template = torch.add(template, mask)
+        # clip at value
+        template = template.clamp(0, value)
         return template
 
     def forward(self):
@@ -83,51 +86,41 @@ class RenderNet(nn.Module):
 
         x0, y0 = self.x0, self.y0
         x1, y1 = self.x1, self.y1
-        _x1, _y1 = self._x1, self._y1
-        x1.register_hook(print)
-        y1.register_hook(print)
+        _x0, _y0 = int(x0.data[0]), int(y0.data[0])
+        _x1, _y1 = int(x1.data[0]), int(y1.data[0])
 
-        # to be differentiable, make 1d first
-        template = Variable(torch.zeros(imsize, imsize))
-        template = self.smooth_index(template, x1, y1, _x1, _y1)
+        kernel = self.gen_kernel()  # to make smooth
+        template = Variable(torch.zeros(1, 1, imsize, imsize))
 
         # start bresenham's line algorithm
-        # steep = False
-        # sx = 1 if torch.gt(x1, x0).data[0] > 0 else -1
-        # sy = 1 if torch.gt(y1, y0).data[0] > 0 else -1
-        # dx = torch.abs(x1 - x0)
-        # dy = torch.abs(y1 - y0)
+        steep = False
+        sx = 1 if torch.gt(x1, x0).data[0] > 0 else -1
+        sy = 1 if torch.gt(y1, y0).data[0] > 0 else -1
+        dx = torch.abs(x1 - x0)
+        dy = torch.abs(y1 - y0)
 
-        # if torch.gt(dy, dx).data[0]:
-        #     steep = True
-        #     x0, y0 = y0, x0
-        #     dx, dy = dy, dx
-        #     sx, sy = sy, sx
+        if torch.gt(dy, dx).data[0]:
+            steep = True
+            x0, y0 = y0, x0
+            dx, dy = dy, dx
+            sx, sy = sy, sx
 
-        # d = (2 * dy) - dx
-        # for i in range(0, int(dx.data[0])):
-        #     idx = mat2vecidx(y0, x0) if steep else mat2vecidx(x0, y0)
-        #     idx = idx.long()
-        #     template[idx] = 1
-        #     while d.data[0] >= 0:
-        #         y0 += sy
-        #         d -= (2 * dx)
-        #     x0 += sx
-        #     d += (2 * dy)
-        # idx = mat2vecidx(x1, y1).long()
-        # template[idx] = 1
-        # template.register_hook(print)
+        d = (2 * dy) - dx
+        for i in range(0, int(dx.data[0])):
+            if steep:
+                template = self.smooth_index(template, kernel,
+                                             y0, x0, _y0, _x0, 1)
+            else:
+                template = self.smooth_index(template, kernel,
+                                             x0, y0, _x0, _y0, 1)
+            while d.data[0] >= 0:
+                y0 += sy
+                d -= (2 * dx)
+            x0 += sx
+            d += (2 * dy)
 
-        # # reshape into matrix
-        template = torch.unsqueeze(template, dim=0)  # add 3rd dim
-        template = torch.cat([template, template, template], dim=0)  # concat
-        template = torch.unsqueeze(template, dim=0)  # add 4th dim
-        # # convolve against kernel to smooth
-        # template = F.conv2d(template, kernel, padding=linewidth // 2)
-
-        # # normalize the matrix to be from 0 and 1
-        template_min = torch.min(template).expand_as(template)
-        template_max = torch.max(template).expand_as(template)
-        template = (template - template_min) / (template_max - template_min)
+        template = self.smooth_index(template, kernel,
+                                     x1, y1, _x1, _y1, 1)
+        template = torch.cat([template, template, template], dim=1)
 
         return template

generative/linerendertest.py
@@ -15,7 +15,7 @@ import torchvision.models as models
 from torch.autograd import Variable
 import torchvision.transforms as transforms
 
-from pix2sketch import (sketch_loss, sample_endpoint_gaussian2d)
+from pix2sketch import (mean_semantic_sketch_loss, sample_endpoint_gaussian2d)
 from linerender import RenderNet
 from vggutils import (VGG19Split, vgg_convert_to_avg_pool)
 
@@ -62,11 +62,13 @@ if __name__ == "__main__":
         optimizer.zero_grad()
         sketch = renderer()
         sketch_embs = vgg19(sketch)
-        loss = sketch_loss(natural_emb, sketch_embs, distractor_embs)
+        loss = mean_semantic_sketch_loss(natural_emb, sketch_embs, distractor_embs)
         loss.backward()
         optimizer.step()
-        print('Train Epoch: {} \tLoss: {:.6f} \tParams: ({}, {})'.format(
-            epoch, loss.data[0], params[0].data[0], params[1].data[0]))
+        print('Train Epoch: {} \tLoss: {:.6f}: \tGrad: ({}, {}) \tParams: ({}, {})'.format(
+            epoch, loss.data[0], params[0].grad.data.numpy()[0],
+            params[1].grad.data.numpy()[0],
+            params[0].data.numpy()[0], params[1].data.numpy()[0]))
 
-    for i in range(5):
+    for i in range(30):
         train(i)

generative/pix2sketch.py
@@ -23,7 +23,54 @@ ALLOWABLE_SAMPLING_PRIORS = ['gaussian', 'angle']
 ALLOWABLE_ACTIONS = ['draw', 'move']
 
 
-def sketch_loss(natural_emb, sketch_embs, distractor_embs, segment_cost=0.0):
+def mean_pixel_sketch_loss(natural_image, sketch_images, distractor_images,
+                           segment_cost=0.0):
+    losses = pixel_sketch_loss(natural_image, sketch_images, distractor_images,
+                               segment_cost=segment_cost)
+    return torch.mean(losses)
+
+
+def pixel_sketch_loss(natural_image, sketch_images, distractor_images,
+                      segment_cost=0.0):
+    """Calculate L2 distance between natural image and sketch
+    image + normalization using distractor images.
+
+    :param natural_image: PyTorch Tensor 1xCxHxW
+    :param sketch_images: PyTorch Tensor SxCxHxW
+    :param distractor_images: PyTorch Tensor DxCxHxW
+    :param segment_cost: cost of adding this segment
+    """
+    n_sketches = sketch_images.size()[0]
+    n_distractors = distractor_images.size()[0]
+
+    # flatten images into a vector
+    natural_image = natural_image.view(1, -1)
+    sketch_images = sketch_images.view(n_sketches, -1)
+    distractor_images = distractor_images.view(n_distractors, -1)
+
+    # distance between natural image and each sketch
+    loss = torch.sum(torch.pow(natural_image - sketch_images, 2), dim=1)
+    loss = torch.unsqueeze(loss, dim=0)
+
+    # distance between distractor image and each sketch
+    distraction_dists = Variable(torch.zeros((n_distractors, n_sketches)))
+    for i in range(n_distractors):
+        distraction_dists[i] = torch.sum(torch.pow(distractor_images[i]
+                                                   - sketch_images, 2), dim=1)
+    all_dists = torch.cat((loss, distraction_dists))
+    norm = torch.norm(all_dists, p=2, dim=0)
+    loss = loss / norm + segment_cost
+    return loss
+
+
+def mean_semantic_sketch_loss(natural_emb, sketch_embs, distractor_embs,
+                              segment_cost=0.0):
+    losses = semantic_sketch_loss(natural_emb, sketch_embs, distractor_embs,
+                                  segment_cost=segment_cost)
+    return torch.mean(losses)
+
+
+def semantic_sketch_loss(natural_emb, sketch_embs, distractor_embs, segment_cost=0.0):
     """Calculate distance between natural image and sketch image
     where the distance is normalized by distractor images.
 
@@ -32,11 +79,11 @@ def sketch_loss(natural_emb, sketch_embs, distractor_embs, segment_cost=0.0):
     :distractor_embs: tuple of PyTorch Variables
     :param segment_cost: cost of adding this segment
     """
-
-    loss = Variable(torch.FloatTensor(1).zero_(), requires_grad=True)
     n_sketches = sketch_embs[0].size()[0]
     n_features = len(natural_emb)
     n_distractors = distractor_embs[0].size()[0]
+
+    loss = Variable(torch.FloatTensor(n_sketches).zero_(), requires_grad=True)
 
     for f in range(n_features):
         costs = F.cosine_similarity(natural_emb[f], sketch_embs[f], dim=1)
@@ -291,8 +338,8 @@ def train(natural_image, distractor_images, **kwargs):
 
             # compute loss functions
             sketch_embs = vgg19(sketches)
-            sketch_loss = SketchLoss(n_features, n_distractors, segment_cost=segment_cost)
-            losses = sketch_loss(natural_emb, sketch_embs, distractor_embs)
+            losses = semantic_sketch_loss(natural_emb, sketch_embs, distractor_embs,
+                                          segment_cost=segment_cost)
             print('- computed losses')
 
             if b == 0: