989cb342263d7f6094f5708bc2597d7871e6ef25
Mike Wu <me@mikewuis.me>
2017-09-13 12:12:58 -0700

    Bugfixes to Spline Renderer

--- generative/splinerender.py
+++ generative/splinerender.py
@@ -3,7 +3,11 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 
+import sys
 import copy
+import numpy as np
+
+
 import torch
 import torch.nn as nn
 
@@ -30,25 +34,25 @@ class SplineRenderNet(nn.Module):
     """
     def __init__(self, x0, y0, x1, y1, x2, y2, imsize=224, fuzz=1, use_cuda=False):
         super(SplineRenderNet, self).__init__()
-            if use_cuda:
-                self.x0 = Variable(torch.cuda.FloatTensor([x0]))
-                self.y0 = Variable(torch.cuda.FloatTensor([y0]))
-                self.x1 = Parameter(torch.cuda.FloatTensor([x1]))
-                self.y1 = Parameter(torch.cuda.FloatTensor([y1]))
-                self.x2 = Parameter(torch.cuda.FloatTensor([x2]))
-                self.y2 = Parameter(torch.cuda.FloatTensor([y2]))
-            else:
-                self.x0 = Variable(torch.FloatTensor([x0]))
-                self.y0 = Variable(torch.FloatTensor([y0]))
-                self.x1 = Parameter(torch.FloatTensor([x1]))
-                self.y1 = Parameter(torch.FloatTensor([y1]))
-                self.x1 = Parameter(torch.FloatTensor([x2]))
-                self.y1 = Parameter(torch.FloatTensor([y2]))
-            self.imsize = imsize
-            self.fuzz = fuzz
-            self.use_cuda = use_cuda
+        if use_cuda:
+            self.x0 = Variable(torch.cuda.FloatTensor([x0]))
+            self.y0 = Variable(torch.cuda.FloatTensor([y0]))
+            self.x1 = Parameter(torch.cuda.FloatTensor([x1]))
+            self.y1 = Parameter(torch.cuda.FloatTensor([y1]))
+            self.x2 = Parameter(torch.cuda.FloatTensor([x2]))
+            self.y2 = Parameter(torch.cuda.FloatTensor([y2]))
+        else:
+            self.x0 = Variable(torch.FloatTensor([x0]))
+            self.y0 = Variable(torch.FloatTensor([y0]))
+            self.x1 = Parameter(torch.FloatTensor([x1]))
+            self.y1 = Parameter(torch.FloatTensor([y1]))
+            self.x1 = Parameter(torch.FloatTensor([x2]))
+            self.y1 = Parameter(torch.FloatTensor([y2]))
+        self.imsize = imsize
+        self.fuzz = fuzz
+        self.use_cuda = use_cuda
 
-        def forward(self):
+    def forward(self):
         template = draw_spline(self.x0, self.y0, self.x1, self.y1, self.x2, self.y2,
                                imsize=self.imsize, fuzz=self.fuzz,
                                use_cuda=self.use_cuda)
@@ -63,18 +67,22 @@ class ParabolicBrezierRenderNet(object):
     After we learn the parameters we should use this to render the final 
     image so that it will look cleaner.
     """
-    def __init__(self, x_list, y_list, tan_x_list, tan_y_list, 
-                 imsize=224, linewidth=1, tstep=0.01):
+    def __init__(self, x0_list, y0_list, x1_list, y1_list,
+                 x2_list, y2_list,  imsize=224, linewidth=1, tstep=0.01):
         super(ParabolicBrezierRenderNet, self).__init__()
-        assert len(x_list) == len(y_list)
-        assert len(tan_x_list) == len(tan_y_list)
-        assert len(x_list) - 1 == len(tan_x_list)
+        assert len(x0_list) == len(y0_list)
+        assert len(x1_list) == len(y1_list)
+        assert len(x2_list) == len(y2_list)
+        assert len(x0_list) == len(x1_list)
+        assert len(x0_list) == len(x2_list)
         assert tstep > 0 and tstep < 1
-        self.n_points = len(x_list)
-        self.x_list = x_list
-        self.y_list = y_list
-        self.tan_x_list = tan_x_list
-        self.tan_y_list = tan_y_list
+        self.n_points = len(x0_list)
+        self.x0_list = x0_list
+        self.y0_list = y0_list
+        self.x1_list = x1_list
+        self.y1_list = y1_list
+        self.x2_list = x2_list
+        self.y2_list = y2_list
         self.imsize = imsize
         if linewidth % 2 == 0:
             linewidth += 1
@@ -83,14 +91,13 @@ class ParabolicBrezierRenderNet(object):
 
     def forward(self):
         template = torch.zeros(self.imsize, self.imsize)
-        for i in range(1, self.n_points):
-            x0 = self.x_list[i - 1]
-            y0 = self.y_list[i - 1]
-            x2 = self.x_list[i]
-            y2 = self.y_list[i]
-            x1 = self.tan_x_list[i - 1]
-            y1 = self.tan_y_list[i - 1]
-
+        for i in range(self.n_points):
+            x0 = self.x0_list[i]
+            y0 = self.y0_list[i]
+            x1 = self.x1_list[i]
+            y1 = self.y1_list[i]
+            x2 = self.x2_list[i]
+            y2 = self.y2_list[i]
             _template = draw_binary_spline(x0, y0, x1, y1, x2, y2, imsize=self.imsize,
                                            width=self.linewidth, tstep=self.tstep)
             template += _template
@@ -176,7 +183,7 @@ def find_closest_point_on_spline(x0, y0, x1, y1, x2, y2, xp, yp):
     c = 2 * (A_x * A_x + A_y * A_y) + pos_x * B_x + pos_y * B_y
     d = pos_x * A_x + pos_y * A_y
     solution = solve_cubic_polynomial(a, b, c, d)
-    ix_not_nan = is_nan(solution)
+    ix_not_nan = 1 - is_nan(solution)
     
     d0 = euclidean_dist(xp, yp, x0, y0)
     d2 = euclidean_dist(xp, yp, x2, y2)
@@ -186,13 +193,18 @@ def find_closest_point_on_spline(x0, y0, x1, y1, x2, y2, xp, yp):
     t_min = torch.ones(n) * sys.maxint
     pos_min_x = torch.ones(n) * np.nan
     pos_min_y = torch.ones(n) * np.nan
-    try_endpoints = torch.zeros(n)
+    try_endpoints = torch.zeros(n).byte()
     
     n_nan = torch.sum(ix_not_nan)
     if n_nan < n * 3:  # has at least 1 root
         # find closest point
         for i in range(3):
-            root = solutions[:, i]
+            if sum(ix_not_nan[:, i]) == 0:
+                break
+
+            root = solution[:, i]
+            root_not_nan = root[ix_not_nan[:, i]]
+
             ix_ge_0 = root_not_nan >= 0
             ix_le_1 = root_not_nan <= 1
             ix_ge_not_nan_gt_0_le_1 = ix_ge_0 + ix_le_1 + ix_not_nan[:, i]
@@ -206,17 +218,24 @@ def find_closest_point_on_spline(x0, y0, x1, y1, x2, y2, xp, yp):
                 
                 ix_best = dist < dist_min[ix_ge_not_nan_gt_0_le_1]
                 if sum(ix_best) > 0:
-                    dist_min[ix_ge_not_nan_gt_0_le_1][ix_best] = dist[ix_best]
-                    t_min[ix_ge_not_nan_gt_0_le_1][ix_best] = root_ge_not_nan_gt_0_le_1[ix_best]
-                    pos_min_x[ix_ge_not_nan_gt_0_le_1][ix_best] = pos_x[ix_best]
-                    pos_min_y[ix_ge_not_nan_gt_0_le_1][ix_best] = pos_y[ix_best]
+                    # ix_update is equiv. to ix_best but |ix_update| = |dist_min|
+                    # where |ix_best| = |ix_ge_not_nan_gt_0_le_1|
+                    ix_update = ix_ge_not_nan_gt_0_le_1
+                    ix_update[ix_best] = 1
+                    ix_update[1 - ix_best] = 0
+
+                    dist_min[ix_update] = dist[ix_best]
+                    t_min[ix_update] = root_ge_not_nan_gt_0_le_1[ix_best]
+                    pos_min_x[ix_update] = pos_x[ix_best]
+                    pos_min_y[ix_update] = pos_y[ix_best]
         
         ix_t_min_neq_maxint = t_min != sys.maxint
         ix_dist_min_lt_d0 = dist_min < d0
         ix_dist_min_lt_d2 = dist_min < d2
-        try_endpoints += (ix_t_min_neq_maxint + ix_dist_min_lt_d0 + ix_dist_min_lt_d2)
+        ix_triplet = ix_t_min_neq_maxint + ix_dist_min_lt_d0 + ix_dist_min_lt_d2
+        try_endpoints = try_endpoints + ix_triplet
         try_endpoints = 1 - torch.clamp(try_endpoints, 0, 1)
-        # eq 1 if we want to try to find a point for it...
+        # equals 1 if we want to try to find an endpoint
         
     if sum(try_endpoints) > 0:
         # if we've reached here, then closest point is on 2 end points
@@ -241,7 +260,7 @@ def find_closest_point_on_spline(x0, y0, x1, y1, x2, y2, xp, yp):
             pos_min_x[ix_try_d0_ge_d2] = x2[ix_try_d0_ge_d2]
             pos_min_y[ix_try_d0_ge_d2] = y2[ix_try_d0_ge_d2]
     
-    return (pox_min_x, pos_min_y)
+    return (pos_min_x, pos_min_y)
 
 
 def solve_cubic_polynomial(a, b, c, d, eps=0.0000001):
@@ -267,21 +286,24 @@ def solve_cubic_polynomial(a, b, c, d, eps=0.0000001):
     # store all roots across vector
     roots = torch.ones(n, 3) * np.nan
     ix_3 = torch.abs(a) > eps
-    a_3, b_3, c_3, d_3 = a[ix_3], b[ix_3], c[ix_3], d[ix_3]
-    a_2, b_2, c_2, d_2 = a[1 - ix_3], b[1 - ix_3], c[1 - ix_3], d[1 - ix_3]
+    ix_2 = 1 - ix_3
     
     # handle the 3 polynomial cases
-    roots_3 = _solve_cubic_polynomial(a_3, b_3, c_3, d_3, eps=eps)
-
-    # handle the 2 polynomial cases
-    roots_2 = _solve_parabolic_polynomial(a_2, b_2, c_2, d_2, eps=eps)
+    if sum(ix_3) > 0:
+        a_3, b_3, c_3, d_3 = a[ix_3], b[ix_3], c[ix_3], d[ix_3]
+        roots_3 = _solve_cubic_polynomial(a_3, b_3, c_3, d_3, eps=eps)
+        roots[:, 0][ix_3] = roots_3[:, 0]
+        roots[:, 1][ix_3] = roots_3[:, 1]
+        roots[:, 2][ix_3] = roots_3[:, 2]
     
-    roots[:, 0][ix_3] = roots_3[:, 0]
-    roots[:, 1][ix_3] = roots_3[:, 1]
-    roots[:, 2][ix_3] = roots_3[:, 2]
-    roots[:, 0][ix_2] = roots_2[:, 0]
-    roots[:, 1][ix_2] = roots_2[:, 1]
-    roots[:, 2][ix_2] = roots_2[:, 2]
+    # handle the 2 polynomial cases
+    if sum(ix_2) > 0:
+        a_2, b_2, c_2, d_2 = a[ix_2], b[ix_2], c[ix_2], d[ix_2]
+        roots_2 = _solve_parabolic_polynomial(a_2, b_2, c_2, d_2, eps=eps)
+        roots[:, 0][ix_2] = roots_2[:, 0]
+        roots[:, 1][ix_2] = roots_2[:, 1]
+        roots[:, 2][ix_2] = roots_2[:, 2]
+    
     return roots
 
 
@@ -293,32 +315,32 @@ def _solve_cubic_polynomial(a, b, c, d, eps=0.0000001):
     b = c / z
     c = d / z
     # setup for Cardano's formula
-    p = b - a * a / 3
-    q = a * (2 * a * a - 9 * b) / 27 + c
+    p = b - a * a / 3.
+    q = a * (2 * a * a - 9 * b) / 27. + c
     p3 = p * p * p
-    D = q * q + 4 * p3 / 27
-    offset = -a / 3
+    D = q * q + 4 * p3 / 27.
+    offset = -a / 3.
 
     # if D > eps
     ix_D_gt_eps = D > eps
     if sum(ix_D_gt_eps) > 0:
         z_D_gt_eps = torch.sqrt(D[ix_D_gt_eps])
-        u_D_gt_eps = (-q[ix_D_gt_eps] + z_D_gt_eps) / 2
-        v_D_gt_eps = (-q[ix_D_gt_eps] - z_D_gt_eps) / 2
+        u_D_gt_eps = (-q[ix_D_gt_eps] + z_D_gt_eps) / 2.
+        v_D_gt_eps = (-q[ix_D_gt_eps] - z_D_gt_eps) / 2.
 
         # u = u**(1 / 3)  if (u >= 0) else -(-u)**(1 / 3)
         u_D_gt_eps_ge_0 = u_D_gt_eps >= 0
         tmp = u_D_gt_eps[u_D_gt_eps_ge_0]
-        u_D_gt_eps[u_D_gt_eps_ge_0] = tmp**(1 / 3)
+        u_D_gt_eps[u_D_gt_eps_ge_0] = tmp**(1 / 3.)
         tmp = u_D_gt_eps[1 - u_D_gt_eps_ge_0]
-        u_D_gt_eps[1 - u_D_gt_eps_ge_0] = -(-tmp)**(1 / 3)
+        u_D_gt_eps[1 - u_D_gt_eps_ge_0] = -(-tmp)**(1 / 3.)
 
         # v = v**(1 / 3)  if (v >= 0) else -(-v)**(1 / 3)
         v_D_gt_eps_ge_0 = v_D_gt_eps >= 0
         tmp = v_D_gt_eps[v_D_gt_eps_ge_0]
-        v_D_gt_eps[v_D_gt_eps_ge_0] = tmp**(1 / 3)
+        v_D_gt_eps[v_D_gt_eps_ge_0] = tmp**(1 / 3.)
         tmp = v_D_gt_eps[1 - v_D_gt_eps_ge_0]
-        v_D_gt_eps[1 - v_D_gt_eps_ge_0] = -(-tmp)**(1 / 3)
+        v_D_gt_eps[1 - v_D_gt_eps_ge_0] = -(-tmp)**(1 / 3.)
 
         root1 = u_D_gt_eps + v_D_gt_eps + offset[ix_D_gt_eps]
         roots[:, 0][ix_D_gt_eps] = root1
@@ -326,11 +348,11 @@ def _solve_cubic_polynomial(a, b, c, d, eps=0.0000001):
     # if D < -eps
     ix_D_lt_neps = D < -eps
     if sum(ix_D_lt_neps) > 0:
-        u_D_lt_neps = 2 * torch.sqrt(-p[ix_D_lt_neps] / 3)
-        v_D_lt_neps = torch.acos(-torch.sqrt(-27 / p3[ix_D_lt_neps]) * q[ix_D_lt_neps] / 2) / 3
+        u_D_lt_neps = 2 * torch.sqrt(-p[ix_D_lt_neps] / 3.)
+        v_D_lt_neps = torch.acos(-torch.sqrt(-27. / p3[ix_D_lt_neps]) * q[ix_D_lt_neps] / 2.) / 3.
         root1 = u * torch.cos(v_D_lt_neps) + offset[ix_D_lt_neps]
-        root2 = u * torch.cos(v_D_lt_neps + 2 * math.pi / 3) + offset[ix_D_lt_neps]
-        root3 = u * torch.cos(v_D_lt_neps + 4 * math.pi / 3) + offset[ix_D_lt_neps]
+        root2 = u * torch.cos(v_D_lt_neps + 2 * math.pi / 3.) + offset[ix_D_lt_neps]
+        root3 = u * torch.cos(v_D_lt_neps + 4 * math.pi / 3.) + offset[ix_D_lt_neps]
 
         roots[:, 0][ix_D_lt_neps] = root1
         roots[:, 1][ix_D_lt_neps] = root2
@@ -347,8 +369,8 @@ def _solve_cubic_polynomial(a, b, c, d, eps=0.0000001):
         u_D_near_0 = torch.zeros(q_D_near_0.size())
     
         ix_D_near_0_q_lt_0 = q_D_near_0 < 0
-        u_D_near_0[ix_D_near_0_q_lt_0] = (-q_D_near_0[ix_D_near_0_q_lt_0] / 2)**(1 / 3)
-        u_D_near_0[1 - ix_D_near_0_q_lt_0] = -(q_D_near_0[1 - ix_D_near_0_q_lt_0] / 2)**(1 / 3)
+        u_D_near_0[ix_D_near_0_q_lt_0] = (-q_D_near_0[ix_D_near_0_q_lt_0] / 2.)**(1 / 3.)
+        u_D_near_0[1 - ix_D_near_0_q_lt_0] = -(q_D_near_0[1 - ix_D_near_0_q_lt_0] / 2.)**(1 / 3.)
 
         root1 = 2 * u_D_near_0 + offset[ix_D_near_0]
         root2 = -u_D_near_0 + offset[ix_D_near_0]
@@ -383,8 +405,8 @@ def _solve_parabolic_polynomial(a, b, c, d, eps=0.0000001):
     # handle case when D is gt eps
     ix_D_gt_eps = D > eps
     if sum(ix_D_gt_eps) > 0:
-        root1 = (-b[ix_D_gt_eps] - torch.pow(D[ix_D_gt_eps], 0.5)) / (2 * a[ix_D_gt_eps])
-        root2 = (-b[ix_D_gt_eps] + torch.pow(D[ix_D_gt_eps], 0.5)) / (2 * a[ix_D_gt_eps])
+        root1 = (-b[ix_D_gt_eps] - torch.pow(D[ix_D_gt_eps], 0.5)) / (2. * a[ix_D_gt_eps])
+        root2 = (-b[ix_D_gt_eps] + torch.pow(D[ix_D_gt_eps], 0.5)) / (2. * a[ix_D_gt_eps])
         roots[:, 0][ix_D_gt_eps] = root1
         roots[:, 1][ix_D_gt_eps] = root2
     
@@ -395,7 +417,7 @@ def _solve_parabolic_polynomial(a, b, c, d, eps=0.0000001):
     ix_D_near_0 = ix_D_near_0 == 2
     
     if sum(ix_D_near_0) > 0:
-        root = -b[ix_D_near_0] / (2 * a[ix_D_near_0])
+        root = -b[ix_D_near_0] / (2. * a[ix_D_near_0])
         roots[:, 0][ix_D_near_0] = root
     
     return roots
