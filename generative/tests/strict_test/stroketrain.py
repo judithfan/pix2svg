5ae9c26800baddd6ca4fae5c9515c225bac9e284
Mike Wu <me@mikewuis.me>
2017-10-17 10:33:18 -0700

    Added script to train using stroke generator

---
+++ generative/tests/strict_test/stroketrain.py
@@ -0,0 +1,133 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+from convmodel import EmbedNet
+from convmodel import save_checkpoint
+from generators2 import DropStrokeGenerator
+
+from train import AverageMeter
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('photo_emb_dir', type=str)
+    parser.add_argument('sketch_emb_dir', type=str)
+    parser.add_argument('out_dir', type=str)
+    parser.add_argument('--drop_proba', type=float, default=0.5)
+    parser.add_argument('--batch_size', type=int, default=10)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--cuda', action='store_true', default=False)
+    args = parser.parse_args()
+    args.cuda = args.cuda and torch.cuda.is_available()
+
+    def reset_generators():
+        train_generator = DropStrokeGenerator(args.photo_emb_dir, args.sketch_emb_dir,
+                                              batch_size=args.batch_size, train=True,
+                                              use_cuda=args.cuda, drop_proba=args.drop_proba)
+        test_generator = DropStrokeGenerator(args.photo_emb_dir, args.sketch_emb_dir,
+                                             batch_size=args.batch_size, train=False,
+                                             use_cuda=args.cuda, drop_proba=args.drop_proba)
+        return train_generator, test_generator
+
+    train_generator, test_generator = reset_generators()
+    train_examples = train_generator.make_generator()
+    test_examples = test_generator.make_generator()
+
+    model = EmbedNet()
+    if args.cuda:
+        model.cuda()
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+
+    def train(epoch):
+        loss_meter = AverageMeter()
+        acc_meter = AverageMeter()
+
+        model.train()
+        batch_idx = 0
+
+        while True: 
+            try:
+                photos, sketches, labels = train_examples.next()
+                batch_idx += 1
+            except StopIteration:
+                break
+
+            optimizer.zero_grad()
+            outputs = model(photos, sketches)
+            loss = F.binary_cross_entropy(outputs.squeeze(1), labels.float())
+            loss_meter.update(loss.data[0], photos.size(0)) 
+
+            # break tape and calculate accuracy
+            labels_np = labels.cpu().data.numpy()
+            outputs_np = np.round(outputs.cpu().squeeze(1).data.numpy(), 0)
+            acc = accuracy_score(labels_np, outputs_np)
+            acc_meter.update(acc, photos.size(0))
+
+            # compute gradients + take a step
+            loss.backward()
+            optimizer.step()
+
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAcc: {:.6f}'.format(
+                  epoch, batch_idx * args.batch_size, train_generator.size,
+                  (100. * batch_idx * args.batch_size) / train_generator.size,
+                  loss_meter.avg, acc_meter.avg))
+
+
+    def test(epoch):
+        acc_meter = AverageMeter()
+        loss_meter = AverageMeter()
+        model.eval()
+        batch_idx = 0
+
+        while True:
+            try:
+                photos, sketches, labels = test_examples.next()
+                batch_idx += 1
+            except StopIteration:
+                break
+            
+            outputs = model(photos, sketches)
+            loss = F.binary_cross_entropy(outputs.squeeze(1), labels.float())
+            loss_meter.update(loss.data[0], photos.size(0))
+
+            labels_np = labels.cpu().data.numpy()
+            outputs_np = np.round(outputs.cpu().squeeze(1).data.numpy(), 0)
+            acc = accuracy_score(labels_np, outputs_np)
+            acc_meter.update(acc, photos.size(0))
+
+        print('Test Epoch: {}\tLoss: {:.6f}\tAcc: {:.6f}'.format(
+              epoch, loss_meter.avg, acc_meter.avg))
+        return acc_meter.avg
+
+
+    best_acc = 0
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        acc = test(epoch)
+
+        train_generator, test_generator = reset_generators()
+        train_examples = train_generator.make_generator()
+        test_examples = test_generator.make_generator()
+
+        is_best = acc > best_acc
+        best_acc = max(acc, best_acc)
+
+        save_checkpoint({
+            'state_dict': model.state_dict(),
+            'best_acc': best_acc,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best, folder=args.out_dir)
+            
