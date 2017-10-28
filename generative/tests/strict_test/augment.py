f157e5fe59a4a8882f607c9094b3712738f64ba9
Mike Wu <me@mikewuis.me>
2017-10-28 16:30:49 -0700

    we now support augmenting by photo and by sketch

--- generative/tests/strict_test/augment.py
+++ generative/tests/strict_test/augment.py
@@ -125,9 +125,8 @@ def gen_crop_name(name, crop_ix):
     return '_'.join(parts)
 
 
-if __name__ == "__main__":
+def augment_by_sketch():
     data_dir = '/data/jefan/sketchpad_basic_fixedpose_augmented'
-    files = os.listdir(os.path.join(data_dir, 'sketch'))
 
     with open(os.path.join(data_dir, 'incorrect_trial_paths_pilot2.txt')) as fp:
         bad_games = fp.readlines()
@@ -170,7 +169,7 @@ if __name__ == "__main__":
             new_csv_data.append(new_row)
 
         print('Progress: [{}/{}] files finished augmentation regime.'.format(
-            ix+1, len(files)))
+            ix+1, len(csv_data)))
 
     with open(os.path.join(data_dir, 'incorrect_trial_paths_pilot2.txt'), 'a') as fp:
         fp.writelines(new_bad_games)
@@ -183,3 +182,103 @@ if __name__ == "__main__":
             writer.writerow(row)
 
     print('Wrote new examples to sketchpad_basic_pilot2_group_data.csv file.')
+
+
+def augment_by_photo():
+    data_dir = '/data/jefan/sketchpad_basic_fixedpose_augmented2'
+
+    with open(os.path.join(data_dir, 'incorrect_trial_paths_pilot2.txt')) as fp:
+        bad_games = fp.readlines()
+        bad_games = [i.replace('.png\n', '.png') for i in bad_games]
+        new_bad_games = []
+
+    with open(os.path.join(data_dir, 'sketchpad_basic_pilot2_group_data.csv')) as fp:
+        reader = csv.reader(fp)
+        csv_data = []
+        new_csv_data = []
+        for row in reader:
+            csv_data.append(row)
+        # open csv data and load data
+        header = csv_data[0]
+        csv_data = csv_data[1:]
+
+    for ix, row in enumerate(csv_data):
+        sketch_path = 'gameID_{id}_trial_{trial}.png'.format(id=row[1], trial=row[2])
+        target_path = 'gameID_{id}_trial_{trial}_{target}.png'.format(id=row[1], trial=row[2], target=row[4])
+        distractor1_path = 'gameID_{id}_trial_{trial}_{target}.png'.format(id=row[1], trial=row[2], target=row[12])
+        distractor2_path = 'gameID_{id}_trial_{trial}_{target}.png'.format(id=row[1], trial=row[2], target=row[13])
+        distractor3_path = 'gameID_{id}_trial_{trial}_{target}.png'.format(id=row[1], trial=row[2], target=row[14])
+
+        target = load(os.path.join(data_dir, 'target', target_path))
+        distractor1 = load(os.path.join(data_dir, 'distractor1', distractor1_path))
+        distractor2 = load(os.path.join(data_dir, 'distractor2', distractor2_path))
+        distractor3 = load(os.path.join(data_dir, 'distractor3', distractor3_path))
+        target_crops = obscure(target, color=127, filter_size=50)
+        distractor1_crops = obscure(distractor1, color=127, filter_size=50)
+        distractor2_crops = obscure(distractor2, color=127, filter_size=50)
+        distractor3_crops = obscure(distractor3, color=127, filter_size=50)
+        crops = zip(target_crops, distractor1_crops, distractor2_crops, distractor3_crops)
+
+        for i, target_crop in enumerate(target_crops):
+            target_im = Image.fromarray(target_crop)
+            target_crop_path = gen_crop_name(target_path, i)
+            target_im.save(os.path.join(data_dir, 'target', target_crop_path))
+
+            # for each target, randomly sample a distractor crop
+            distractor1_crop = random.choice(distractor1_crops)
+            distractor2_crop = random.choice(distractor2_crops)
+            distractor3_crop = random.choice(distractor3_crops)
+
+            distractor1_im = Image.fromarray(distractor1_crop)
+            distractor2_im = Image.fromarray(distractor2_crop)
+            distractor3_im = Image.fromarray(distractor3_crop)
+
+            distractor1_crop_path = gen_crop_name(distractor1_path, i)
+            distractor2_crop_path = gen_crop_name(distractor2_path, i)
+            distractor3_crop_path = gen_crop_name(distractor3_path, i)
+
+            distractor1_im.save(os.path.join(data_dir, 'distractor1', distractor1_path))
+            distractor2_im.save(os.path.join(data_dir, 'distractor2', distractor2_path))
+            distractor3_im.save(os.path.join(data_dir, 'distractor3', distractor3_path))
+
+            # clone sketch whole
+            sketch_crop_path = gen_crop_name(sketch_path, i)
+            shutil.copy(os.path.join(data_dir, 'sketch', sketch_path), 
+                        os.path.join(data_dir, 'sketch', sketch_crop_path))
+
+            # if we crop a bad sketch, make sure to record cropped sketches as bad as well
+            if sketch_path in bad_games:
+                new_bad_games.append(sketch_crop_path.replace('.png', '.png\n'))
+
+            # add a new row in the CSV for each cropped image
+            new_row = copy.deepcopy(row)
+            new_row[1] = row[1] + '-crop%d' % i
+            new_csv_data.append(new_row)
+
+        print('Progress: [{}/{}] files finished augmentation regime.'.format(
+            ix+1, len(csv_data)))
+
+    with open(os.path.join(data_dir, 'incorrect_trial_paths_pilot2.txt'), 'a') as fp:
+        fp.writelines(new_bad_games)
+
+    print('Wrote new examples to incorrect_trial_paths_pilot2.txt file.')
+
+    with open(os.path.join(data_dir, 'sketchpad_basic_pilot2_group_data.csv'), 'a') as fp:
+        writer = csv.writer(fp)
+        for row in new_csv_data:
+            writer.writerow(row)
+
+    print('Wrote new examples to sketchpad_basic_pilot2_group_data.csv file.')
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('augment', type=str, help='photo|sketch')
+    args = parser.parse_args()
+    assert args.augment in ['photo', 'sketch']
+
+    if args.augment == 'sketch':
+        augment_by_sketch()
+    elif args.augment == 'photo':
+        augment_by_photo()
