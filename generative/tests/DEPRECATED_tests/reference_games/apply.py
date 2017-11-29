d6c16df90f62d5a28cf9673748a9353fdfb92d42
Mike Wu <me@mikewuis.me>
2017-11-29 14:09:10 -0800

    Move reference_games folder to DEPRECATED files

---
+++ generative/tests/DEPRECATED_tests/reference_games/apply.py
@@ -0,0 +1,85 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import sys
+import json
+import torch
+
+from generators import ReferenceGameEmbeddingGenerator
+
+sys.path.append('..')
+import ranking_test.model as rk_utils
+import multimodal_test.model as mm_utils
+from multimodal_test.utils import cosine_similarity
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sketch_emb_dir', type=str, help='path to sketches')
+    parser.add_argument('render_emb_dir', type=str, help='path to renderings')
+    parser.add_argument('json_path', type=str, help='path to where to dump json')
+    # if this is supplied, then we know the model we are using is a ranking model,
+    # which will affect our distance calculation.
+    parser.add_argument('--ranking', action='store_true', default=False,
+                        help='if supplied, assume we are loading ranking_test + euclidean.')
+    # if this is not supplied, we can calculate pearson correlation directly 
+    # on the VGG embeddings themselves.
+    parser.add_argument('--model_dir', type=str, help='path to trained MM model')
+    parser.add_argument('--cuda', action='store_true', default=False)
+    args = parser.parse_args()
+    args.cuda = args.cuda and torch.cuda.is_available()
+
+    generator = ReferenceGameEmbeddingGenerator(args.sketch_emb_dir, args.render_emb_dir, 
+                                                use_cuda=args.cuda)
+    examples = generator.make_generator() 
+    print('Built generator.')
+
+    # load multimodal model  
+    if args.model_dir:
+        if args.ranking:
+            model = rk_utils.load_checkpoint(args.model_dir, use_cuda=args.cuda)
+            print('Loaded ranking model.')
+        else:
+            model = mm_utils.load_checkpoint(args.model_dir, use_cuda=args.cuda)
+            print('Loaded multimodal model.')
+        model.eval()
+        if model.cuda:
+            model.cuda()
+        
+    dist_jsons = []
+    count = 0
+
+    while True:
+        try:  # exhaust the generator to return all pairs. this loads 1 tuple at a time.
+            sketch_emb_path, sketch_emb, render_emb_path, render_emb = examples.next()
+        except StopIteration:
+            break
+
+        if args.model_dir:
+            # pass sketch and render in VGG (fc7) and then get MM embeddings
+            # this is the same for our ranking model (luckily)
+            sketch_emb = model.sketch_adaptor(sketch_emb)
+            render_emb = model.photo_adaptor(render_emb)
+
+        if args.model_dir and args.ranking:
+            # our ranking model is trained on euclidean distance.
+            dist = torch.norm(render_emb - sketch_emb, dim=1, p=2, keepdim=True)
+        else:  # compute cosine similarity
+            render_emb = render_emb - torch.mean(render_emb, dim=1, keepdim=True)
+            sketch_emb = sketch_emb - torch.mean(sketch_emb, dim=1, keepdim=True)
+            dist = cosine_similarity(render_emb, sketch_emb, dim=1)
+        dist = float(dist.cpu().data.numpy()[0])
+        dist_json = {'sketch': sketch_emb_path,
+                     'render': render_emb_path,
+                     'distance': dist}
+        dist_jsons.append(dist_json)
+
+        print('Compute Distance [{}/{}].'.format(count + 1, generator.size))
+        count += 1
+
+
+    print('\nWriting distances to {}.')
+    with open(args.json_path, 'w') as fp:
+        json.dump(dist_jsons, fp)
---
+++ generative/tests/DEPRECATED_tests/reference_games/visualize.R
@@ -0,0 +1,96 @@
+#!/usr/bin/env Rscript
+knitr::opts_chunk$set(echo = TRUE)
+library(tidyverse)
+library(ggthemes)
+library(lme4)
+library(jsonlite)
+library(stringr)
+library(optparse)
+
+
+# Handle command line options
+option_list = list(
+  make_option(c("-j", "--json"), type="character", default=NULL, 
+              help="json file path", metavar="character")
+); 
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+# build data from CSV file
+raw <- read_delim('./data/sketchpad_basic_merged_group_data.csv', delim = ',') 
+d <- raw %>% 
+  mutate(gameid = str_sub(gsub("-", "", gameID), start = -12)) %>%
+  mutate(sketchLabel = sprintf('%s_%s', gameid, trialNum)) %>%
+  filter(iteration == 'pilot1') %>% # Just use pilot 1 for now
+  select(sketchLabel, condition, target, Distractor1, Distractor2, Distractor3, mean_intensity, pose, targetDomain) 
+d$context = apply(d %>% select(Distractor1, Distractor2, Distractor3), 1, 
+                  function(x) paste(sort(x), collapse="_"))
+d.ordered = d %>% 
+  select(-Distractor1, -Distractor2, -Distractor3) %>%
+  separate(context, into = c('Distractor1', 'Distractor2', 'Distractor3')) %>%
+  mutate(Target = sprintf("%s_%04d", target, pose)) %>%
+  mutate(Distractor1 = sprintf("%s_%04d", Distractor1, pose)) %>%
+  mutate(Distractor2 = sprintf("%s_%04d", Distractor2, pose)) %>%
+  mutate(Distractor3 = sprintf("%s_%04d", Distractor3, pose))
+
+similarities <- fromJSON(opt$json, flatten = T)
+lookupSimilarity <- function(object, sketch) {
+  return(similarities[[object]][[sketch]])
+}
+
+d.similarity <- d.ordered %>%
+  gather(contextElement, name, Target, Distractor1, Distractor2, Distractor3) %>%
+  rowwise() %>%
+  mutate(similarity = lookupSimilarity(name, sketchLabel))
+
+# plot density when comparing FAR conditions
+tmp = d.similarity %>% 
+  mutate(contextElement = case_when(contextElement == 'Target' ~ 'target',
+                                    T ~ 'distractor')) %>%
+  filter(condition == 'further') 
+
+tmp %>%
+  ggplot(aes(x = similarity, fill = contextElement)) +
+    geom_density(alpha = .5) +
+    theme_few() +
+    theme(aspect.ratio = 1) +
+    xlim(-1, 1) +
+    ggtitle('sketch similarity in FAR condition') +
+    scale_fill_colorblind()
+
+# break apart FAR by domain
+tmp %>%
+  ggplot(aes(x = similarity, fill = contextElement)) +
+  geom_density(alpha = .5) +
+  theme_few() +
+  theme(aspect.ratio = 1) +
+  xlim(-1, 1) +
+  ggtitle('sketch similarity in FAR condition') +
+  facet_wrap(~ targetDomain) +
+  scale_fill_colorblind()
+
+# plot density when comparing CLOSE conditions
+tmp = d.similarity %>% 
+  mutate(contextElement = case_when(contextElement == 'Target' ~ 'target',
+                                    T ~ 'distractor')) %>%
+  filter(condition == 'closer') 
+
+tmp %>%
+  ggplot(aes(x = similarity, fill = contextElement)) +
+    geom_density(alpha = .5) +
+    theme_few() +
+    theme(aspect.ratio = 1) +
+    xlim(-1, 1) +
+    ggtitle('sketch similarity in CLOSE condition') +
+    scale_fill_colorblind()
+
+# break apart CLOSE by domain
+tmp %>%
+  ggplot(aes(x = similarity, fill = contextElement)) +
+  geom_density(alpha = .5) +
+  theme_few() +
+  theme(aspect.ratio = 1) +
+  xlim(-1, 1) +
+  ggtitle('sketch similarity in CLOSE condition') +
+  facet_wrap(~ targetDomain) +
+  scale_fill_colorblind()
