333ccd129f45f62a53467e416cd2e80ba3d8e098
Mike Wu <me@mikewuis.me>
2017-11-29 15:08:37 -0800

    Remove extra offices in triplet_test

--- generative/tests/triplet_test/datasets.py
+++ generative/tests/triplet_test/datasets.py
@@ -426,31 +426,23 @@ if __name__ == "__main__":
     """Running this will generate the pickle file to run PreloadedGenerator"""
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, help='conv_4_2|fc7', default='conv_4_2')
-    parser.add_argument('--closer', action='store_true', help='if True, include only closer examples')
-    parser.add_argument('--v96', action='store_true', default=False, help='use 96 game version')
+    parser.add_argument('--layer', type=str, help='conv_4_2|fc7', default='conv_4_2')
     parser.add_argument('--photo_augment', action='store_true')
-    parser.add_argument('--sketch_augment', action='store_true')
     args = parser.parse_args()
-    args.v96 = '96' if args.v96 else ''
-    assert args.model in ['conv_4_2', 'fc7']
+    assert args.layer in ['conv_4_2', 'fc7']
     
-    if args.photo_augment and args.sketch_augment:
-        raise Exception('Cannot pass both photo_augment and sketch_augment')
     if args.photo_augment:
-        data_dir = '/data/jefan/sketchpad_basic_fixedpose%s_photo_augmented_%s' % (args.v96, args.model)
-    elif args.sketch_augment:
-        data_dir = '/data/jefan/sketchpad_basic_fixedpose%s_sketch_augmented_%s' % (args.v96, args.model)
+        data_dir = '/data/jefan/sketchpad_basic_fixedpose96_photo_augmented_%s' % args.layer
     else:
-        data_dir = '/data/jefan/sketchpad_basic_fixedpose%s_%s' % (args.v96, args.model)
+        data_dir = '/data/jefan/sketchpad_basic_fixedpose96_%s' % args.layer
 
-    generator = ContextFreeGenerator(closer_only=args.closer, data_dir=data_dir)
-    pickle_name = 'preloaded_context_closer.pkl' if args.closer else 'preloaded_context_all.pkl'
-    with open(os.path.join(data_dir, pickle_name), 'wb') as fp:
+    generator = ContextFreeGenerator(data_dir=data_dir)
+    with open(os.path.join(data_dir, 'preloaded_context_all.pkl'), 'wb') as fp:
         cPickle.dump({'cat2target': generator.cat2target, 
                       'target2sketch': generator.target2sketch,
                       'distractor2sketch': generator.distractor2sketch,
                       'target2distractors': generator.target2distractors,
                       'path2folder': generator.path2folder,
                       'target2condition': generator.target2condition}, fp)
+    
     print('Pickle saved.')
