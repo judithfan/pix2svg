1831451e5703e87adbdf0b33ee7db2c7632f5783
mhw32 <me@mikewuis.me>
2017-12-01 17:58:42 -0800

    Plotting manifold is working now but they don't seem that sensible -- we should divide it up inot separate images

--- generative/tests/conv_test/manifold.py
+++ generative/tests/conv_test/manifold.py
@@ -48,10 +48,10 @@ if __name__ == "__main__":
     n_labels = 32 if args.instance else 4
     ix2name_dict = INSTANCE_IX2NAME_DICT if args.instance else CATEGORY_IX2NAME_DICT
 
-    sketch_embeddings = torch.zeros((generator.size, 1000))
-    photo_embeddings = torch.zeros((generator.size, 1000))
-    sketch_labels = []
-    photo_labels = []
+    sketch_embeddings = torch.zeros((generator.size, 4, 1000))
+    photo_embeddings = torch.zeros((generator.size, 4, 1000))
+    sketch_labels = torch.zeros((generator.size, 4))
+    photo_labels = torch.zeros((generator.size, 4))
     
     batch_idx = 0
     while True:
@@ -64,36 +64,36 @@ if __name__ == "__main__":
             sketch = sketch - torch.mean(sketch, dim=1, keepdim=True)
             photo = photo - torch.mean(photo, dim=1, keepdim=True)
 
-            sketch_embeddings[batch_idx, :] = sketch.cpu().data[0]
-            sketch_embeddings[batch_idx, :] = photo.cpu().data[0]
-
+            sketch_embeddings[batch_idx, :, :] = sketch.cpu().data
+            photo_embeddings[batch_idx, :, :] = photo.cpu().data
             if args.instance:
-                sketch_labels.append(instance[0])
-                photo_labels.append(instance[0])
+                sketch_labels[batch_idx, :] = instance.cpu().data
+                photo_labels[batch_idx, :] = instance.cpu().data
             else:
-                sketch_labels.append(category[0])
-                photo_labels.append(category[0])
+                sketch_labels[batch_idx, :] = category.cpu().data
+                photo_labels[batch_idx, :] = category.cpu().data
 
             batch_idx += 1
             print('Collecting examples [{}/{}].'.format(batch_idx, generator.size))
         except StopIteration:
             break
 
-    sketch_embeddings = sketch_embeddings.numpy()
-    photo_embeddings = photo_embeddings.numpy()
+    sketch_embeddings = sketch_embeddings.view(-1, 1000).numpy()
+    photo_embeddings = photo_embeddings.view(-1, 1000).numpy()
+    sketch_labels = sketch_labels.view(-1).numpy()
+    photo_labels = photo_labels.view(-1).numpy()
+    n_sketch_embeddings = sketch_embeddings.shape[0]
+
     all_embeddings = np.concatenate((sketch_embeddings, photo_embeddings), axis=0)
+    # pca_50 = PCA(n_components=50)
+    # tsne_2 = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+    # all_embeddings = pca_50.fit_transform(all_embeddings)
+    # all_embeddings = tsne_2.fit_transform(all_embeddings)
+    pca_2 = PCA(n_components=2)
+    all_embeddings = pca_2.fit_transform(all_embeddings)
 
-    sketch_labels = np.array(sketch_labels)
-    photo_labels = np.array(photo_labels)
-
-    pca_50 = PCA(n_components=50)
-    tsne_2 = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
-
-    all_embeddings = pca_50.fit_transform(all_embeddings)
-    all_embeddings = tsne_2.fit_transform(all_embeddings)
-
-    sketch_embeddings = all_embeddings[:generator.size, :]
-    photo_embeddings = all_embeddings[generator.size:, :]
+    sketch_embeddings = all_embeddings[:n_sketch_embeddings, :]
+    photo_embeddings = all_embeddings[n_sketch_embeddings:, :]
 
     plt.figure()
     for i in xrange(n_labels):
@@ -103,7 +103,7 @@ if __name__ == "__main__":
                     alpha=0.1, edgecolors='none', label='%s-sketch' % ix2name_dict[i])
         plt.scatter(_photo_embeddings[:, 0], _photo_embeddings[:, 1],
                     alpha=0.1, edgecolors='none', label=ix2name_dict[i])
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.legend()
     plt.tight_layout()
     plt.savefig('./manifold-{}.png'.format(
         'instance' if args.instance else 'category'))
