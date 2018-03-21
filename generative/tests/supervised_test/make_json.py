Bugfix: numerical stabilities with softmax

generative/tests/supervised_test/make_json.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 
 import os
 import sys
+import json
 import shutil
 import numpy as np
 from tqdm import tqdm
@@ -14,20 +15,21 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 from model import SketchNet
-from dataset import SketchPlus32Photos 
+from dataset import SketchPlus32Photos
+from train import load_checkpoint 
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('json_path', type=str, help='path to where to dump json')
-    parser.add_argument('--model-path', type=str, default='./trained_models/checkpoint.pth.tar', 
+    parser.add_argument('--model-path', type=str, default='./trained_models/model_best.pth.tar', 
                         help='where trained models are stored.')
     parser.add_argument('--cuda', action='store_true', default=False) 
     args = parser.parse_args()
     args.cuda = args.cuda and torch.cuda.is_available()
 
-    model = SketchNet()
+    model = load_checkpoint(args.model_path)    
     model.eval()
     if args.cuda:
         model.cuda()
@@ -50,11 +52,12 @@ if __name__ == "__main__":
             sketch = sketch.cuda()
             label = label.cuda()
 
-        distances = model(photos, sketch).cpu().data.numpy()
+        distances = model(photos, sketch)
+        distances = distances.cpu().data.numpy().flatten()
         for i in xrange(32):
-            output = {u'distance': distances[i],
-                      u'render': photo_paths[i],
-                      u'sketch': sketch_path}
+            output = {u'distance': float(distances[i]),
+                      u'render': str(photo_paths[i]),
+                      u'sketch': str(sketch_path)}
             dist_jsons.append(output)
         pbar.update()
     pbar.close()

generative/tests/supervised_test/model.py
@@ -34,7 +34,7 @@ class SketchNet(nn.Module):
         distances = torch.cat([torch.norm(photos[:, i] - sketch, p=2, dim=1).unsqueeze(1) 
                                for i in xrange(self.n_photos)], dim=1)
         # want this to sum to one
-        distances = F.softmax(distances, dim=1)
+        distances = distances / torch.sum(distances, 1).unsqueeze(1)
         return distances
 
 

generative/tests/supervised_test/train.py
@@ -6,6 +6,7 @@ import os
 import sys
 import shutil
 import numpy as np
+from tqdm import tqdm
 
 import torch
 import torch.optim as optim
@@ -18,6 +19,20 @@ from sklearn.metrics import accuracy_score
 from model import SketchNet
 from dataset import SketchPlus32Photos 
 
+def save_checkpoint(state, is_best, folder='./', filename='checkpoint.pth.tar'):
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+    torch.save(state, os.path.join(folder, filename))
+    if is_best:
+        shutil.copyfile(os.path.join(folder, filename),
+                        os.path.join(folder, 'model_best.pth.tar'))
+
+def load_checkpoint(file_path, use_cuda=False):
+    checkpoint = torch.load(file_path) if use_cuda else \
+        torch.load(file_path, map_location=lambda storage, location: storage)
+    model = SketchNet()
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -43,14 +58,16 @@ if __name__ == "__main__":
     parser.add_argument('--out-dir', type=str, default='./trained_models', 
                         help='where to save model [default: ./trained_models]')
     parser.add_argument('--batch-size', type=int, default=64, help='number of examples in a mini-batch [default: 64]')
-    parser.add_argument('--lr', type=float, default=3e-4, help='learning rate [default: 3e-4]')
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate [default: 1e-3]')
     parser.add_argument('--epochs', type=int, default=100, help='number of epochs [default: 100]')
     parser.add_argument('--log-interval', type=int, default=10, help='how frequently to print stats [default: 10]')
     parser.add_argument('--cuda', action='store_true', default=False) 
     args = parser.parse_args()
     args.cuda = args.cuda and torch.cuda.is_available()
 
-    loader = torch.utils.data.DataLoader(SketchPlus32Photos(), batch_size=args.batch_size, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(SketchPlus32Photos(), batch_size=args.batch_size)
+    test_loader = torch.utils.data.DataLoader(SketchPlus32Photos(), batch_size=args.batch_size, shuffle=False)
+
     model = SketchNet() 
     if args.cuda:
         model.cuda()
@@ -58,9 +75,10 @@ if __name__ == "__main__":
     
     def train(epoch):
         model.train()
+        train_loss = 0
         loss_meter = AverageMeter()
 
-        for batch_idx, (photos, sketch, label) in enumerate(loader):
+        for batch_idx, (photos, sketch, label) in enumerate(train_loader):
             photos = Variable(photos)
             sketch = Variable(sketch)
             label = Variable(label, requires_grad=False)
@@ -72,23 +90,54 @@ if __name__ == "__main__":
                 label = label.cuda()
             
             optimizer.zero_grad()
-            
             distances = model(photos, sketch)
             loss = F.mse_loss(distances, label, size_average=False)
             loss_meter.update(loss.data[0], len(photos))
+            train_loss += loss.data[0]
 
             loss.backward()
             optimizer.step()
 
-            if batch_idx & args.log_interval == 0:
+            if batch_idx % args.log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * batch_size,
-                      len(loader.dataset), 100. * batch_idx / len(loader), loss_meter.avg))
-
+                      len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss_meter.avg))
+        
+        train_loss /= len(train_loader)
         print('====> Epoch: {}\tLoss: {:.4f}'.format(epoch, loss_meter.avg))
 
+    def test():
+        model.eval()
+        loss_meter = AverageMeter()
+        test_loss = 0
+        pbar = tqdm(total=len(test_loader))
+        for batch_idx, (photos, sketch, label) in enumerate(test_loader):
+            photos = Variable(photos, volatile=True)
+            sketch = Variable(sketch, volatile=True)
+            label = Variable(label, requires_grad=False)
+            batch_size = len(photos)
+            if args.cuda:
+                photos = photos.cuda()
+                sketch = sketch.cuda()
+                label = label.cuda()
+            distances = model(photos, sketch)
+            loss = F.mse_loss(distances, label, size_average=False)
+            loss_meter.update(loss.data[0], len(photos))
+            test_loss += loss.data[0]
+            pbar.update()
+        pbar.close()
+        print('====> Test Loss: {:.4f}'.format(loss_meter.avg))
+        test_loss /= len(test_loader)
+        return loss_meter.avg
     
     best_loss = sys.maxint
     for epoch in xrange(1, args.epochs + 1):
         train(epoch)
-        torch.save(model.state_dict(), os.path.join(args.out_dir, 'checkpoint.pth.tar'))
+        loss = test()
+        is_best = loss < best_loss
+        best_loss = min(loss, best_loss)
+        save_checkpoint({
+            'state_dict': model.state_dict(),
+            'best_loss': best_loss,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best, folder=args.out_dir)
 