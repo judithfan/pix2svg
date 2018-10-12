4654bafe80d16e2ce935a2e1af28cf838818c671
mhw32 <me@mikewuis.me>
2018-10-12 15:39:40 -0700

    some update

--- generative/tests/compare_test/concat_first/test_average.py
+++ generative/tests/compare_test/concat_first/test_average.py
@@ -10,8 +10,8 @@ import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-from train import load_checkpoint, AverageMeter
-from dataset import VisualDataset
+from train_average import load_checkpoint, AverageMeter
+from dataset import VisualDataset, OBJECT_TO_CATEGORY
 
 
 if __name__ == "__main__":
@@ -32,27 +32,31 @@ if __name__ == "__main__":
     if args.cuda:
         model = model.cuda()
 
-    test_dataset = VisualDataset(layer=model.layer, split='test', average_labels=True, 
-                                 train_test_split_dir=args.train_test_split_dir)
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset, batch_size=args.batch_size, shuffle=False)
+    test_dataset = ExhaustiveDataset(layer=model.layer, split='test',
+                                     train_test_split_dir=args.train_test_split_dir)
+    loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
     model.eval()
-    acc_meter = AverageMeter()
+    object_acc_meter = AverageMeter()
+    category_acc_meter = AverageMeter()
     pbar = tqdm(total=len(test_loader))
 
-    for batch_idx, (sketch, label) in enumerate(test_loader):
+    for batch_idx, (sketch, sketch_object, sketch_context, sketch_path) in enumerate(test_loader):
         sketch = Variable(sketch, volatile=True)
         label = Variable(label.float(), requires_grad=False)
         batch_size = len(sketch)
+        sketch_category = OBJECT_TO_CATEGORY[sketch_object]
 
         if args.cuda:
             sketch = sketch.cuda()
             label = label.cuda()
 
         pred_logits = []
+        photo_objects = []
+        photo_categories = []
         photo_generator = test_dataset.gen_photos()
-        for photo in photo_generator():
+        for photo, photo_object, photo_path in photo_generator():
+            photo_category = OBJECT_TO_CATEGORY[photo_object]
             photo = Variable(photo)
             if args.cuda:
                 photo = photo.cuda()
@@ -60,11 +64,13 @@ if __name__ == "__main__":
                      photo.repeat(batch_size, 1, 1, 1))
             pred_logit = model(photo, sketch)
             pred_logits.append(pred_logit)
+            photo_objects.append(photo_objects)
+            photo_categories.append(photo_category)
 
         pred_logits = torch.cat(pred_logits, dim=1)
-        import pdb;pdb.set_trace()
         pred = F.softmax(pred_logits, dim=1)
         pred = torch.max(pred_logits, dim=1)[1]
+        import pdb; pdb.set_trace()
 
         accuracy = torch.sum(pred == label.long()) / batch_size
         acc_meter.update(accuracy.data[0])
